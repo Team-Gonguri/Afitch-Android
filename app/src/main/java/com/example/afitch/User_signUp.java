9ebd9c86cd3bbe3e69c478560a3eff4a8d1b07c7
박지원 <cute060500@naver.com>
2021-08-03 01:38:08 +0900

    login & signup

--- app/src/main/java/com/example/afitch/User_signUp.java
+++ app/src/main/java/com/example/afitch/User_signUp.java
@@ -2,7 +2,14 @@ package com.example.afitch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class User_signUp extends AppCompatActivity {
 
@@ -10,5 +17,23 @@ public class User_signUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up);
+
+        // logo 에서 I 만 색깔 변경
+        TextView logo = (TextView)findViewById(R.id.a_fitch2);
+        String content = logo.getText().toString();
+        SpannableString spannableString = new SpannableString(content);
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#FF9595")), 3, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        logo.setText(spannableString);
+
+        TextView goto_login = (TextView) findViewById(R.id.goto_login);
+
+        goto_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),User_login.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
