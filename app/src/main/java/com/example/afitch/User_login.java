a22ac4cde1cc5bc46f455bc59e3a1362fc074db7
박지원 <cute060500@naver.com>
2021-08-11 15:48:12 +0900

    로그인 & 회원가입

--- app/src/main/java/com/example/afitch/User_login.java
+++ app/src/main/java/com/example/afitch/User_login.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,7 +28,8 @@ public class User_login extends AppCompatActivity {
     public TextView goto_signup;
     public EditText edit_id, edit_pw;
     public Button goto_login;
-    String id, pw, result;
+    String id, pw;
+    JSONObject temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,7 @@ public class User_login extends AppCompatActivity {
         goto_signup = (TextView) findViewById(R.id.goto_signup);
         edit_id = (EditText) findViewById(R.id.login_id);
         edit_pw = (EditText) findViewById(R.id.login_pw);
+        temp = new JSONObject();
 
         goto_signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,43 +70,52 @@ public class User_login extends AppCompatActivity {
                 }
 
                 // AsyncTask를 통해 HttpURLConnection 수행.
-                NetworkTask networkTask = new NetworkTask(url, values);
+                NetworkTask networkTask = new NetworkTask(url, "post", true, values);
                 networkTask.execute();
+                System.out.println(temp);
             }
         });
     }
 
-    public class NetworkTask extends AsyncTask<Void, Void, String> {
-        private String url;
+    public class NetworkTask extends AsyncTask<Void, Void, JSONObject> {
+        private String url, method;
         private JSONObject values;
+        Boolean response;
 
-        public NetworkTask(String url, JSONObject values) {
+        public NetworkTask(String url,String method, Boolean response, JSONObject values) {
             this.url = url;
+            this.method = method;
             this.values = values;
+            this.response = response;
         }
 
         @Override
-        protected String doInBackground(Void... params) {
+        protected JSONObject doInBackground(Void... params) {
+            JSONObject result;
             Log.d("체크","doInBackground 진입");
-            String result; // 요청 결과를 저장할 변수.
             RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
-            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+            result = requestHttpURLConnection.request(url, method, response, values); // 해당 URL로 부터 결과물을 얻어온다.
             Log.d("체크", "result : " + result);
             return result;
         }
 
         @Override
-        protected void onPostExecute(String s) {
+        protected void onPostExecute(JSONObject s) {
             super.onPostExecute(s); //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
-            if (s == null){
-                Log.d("체크","로그인 실패");
-            }
-            else{
-                // accessToken, refreshToken 다루기
-                /*
-                 */
-                Log.d("체크","로그인 성공");
-                // 홈 화면으로
+            temp = s;
+            try {
+                if (s.getString("status_code").equals("200")){
+                    Toast toast = Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.", Toast.LENGTH_SHORT);
+                    toast.show();
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast toast1 = Toast.makeText(getApplicationContext(),"잘못된 정보입니다. 다시 입력해주세요", Toast.LENGTH_SHORT);
+                    toast1.show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         }
     }
--- app/src/main/java/com/example/afitch/User_signUp.java
+++ app/src/main/java/com/example/afitch/User_signUp.java
@@ -1,5 +1,6 @@
 package com.example.afitch;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
@@ -11,10 +12,12 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,9 +25,11 @@ import org.json.JSONObject;
 public class User_signUp extends AppCompatActivity {
 
     public TextView goto_login;
-    EditText edit_id, edit_pw, edit_height, edit_weight, edit_nickName;
+    EditText edit_id, edit_pw, edit_nickName;
     public Button goto_signup;
-    String id, pw, height, weight, nickName;
+    String id, pw,nickName;
+    String checkIdResponse, checkNickResponse, checkSignupResponse, signup_url, checkId_url, checkNickname_url;
+    JSONObject temp,signup_info, json_nickname,json_id ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +39,9 @@ public class User_signUp extends AppCompatActivity {
         goto_login = (TextView) findViewById(R.id.goto_login);
         edit_id = (EditText) findViewById(R.id.signup_id);
         edit_pw = (EditText) findViewById(R.id.signup_pw);
-        edit_height = (EditText) findViewById(R.id.signup_height);
-        edit_weight = (EditText) findViewById(R.id.signup_weight);
         edit_nickName = (EditText) findViewById(R.id.signup_nickName);
         goto_signup = (Button) findViewById(R.id.signup_btn);
+        temp = new JSONObject();
 
         goto_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,57 +57,125 @@ public class User_signUp extends AppCompatActivity {
                 Log.d("체크","회원가입 버튼 클릭확인");
                 id = edit_id.getText().toString();
                 pw = edit_pw.getText().toString();
-                height = edit_height.getText().toString();
-                weight = edit_weight.getText().toString();
                 nickName = edit_nickName.getText().toString();
-                String url = "http://3.36.65.27:8080/auth/sign-up?role=ROLE_USER";
-                JSONObject values = new JSONObject();
+                signup_url = "http://3.36.65.27:8080/auth/sign-up?role=ROLE_USER";
+                checkId_url = "http://3.36.65.27:8080/auth/id-duplicate?id=";
+                checkNickname_url = "http://3.36.65.27:8080/auth/nickname-duplicate?nickName=";
+                json_id = new JSONObject();
+                json_nickname = new JSONObject();
+                signup_info = new JSONObject();
                 try {
-                    values.put("accountId",id);
-                    values.put("adminCode","");
-                    values.put("nickName",nickName);
-                    values.put("password",pw);
+                    json_id.put("accountId",id);
+                    json_nickname.put("nickName",nickName);
+                    signup_info.put("accountId",id);
+                    signup_info.put("adminCode","");
+                    signup_info.put("nickName",nickName);
+                    signup_info.put("password",pw);
 
-                    System.out.println(values);
-
+                    System.out.println(signup_info);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
 
-                // AsyncTask를 통해 HttpURLConnection 수행.
-                NetworkTask networkTask = new NetworkTask(url, values);
-                networkTask.execute();
+                if (id.equals("") | pw.equals("") | nickName.equals("")){
+                    Toast toast = Toast.makeText(getApplicationContext(),"모든 정보를 입력해주세요.", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else {
+                    // 아이디 중복확인
+                    NetworkTask networkTask = new NetworkTask(checkId_url.concat(id), "get", true, json_id, 1);
+                    networkTask.execute();
+
+                    // 닉네임 중복확인
+                    NetworkTask networkTask2 = new NetworkTask(checkNickname_url.concat(nickName), "get", true, json_nickname, 2);
+                    networkTask2.execute();
+
+                }
             }
         });
     }
 
-    public class NetworkTask extends AsyncTask<Void, Void, String> {
-        private String url;
+    public class NetworkTask extends AsyncTask<Void, Void, JSONObject> {
+        private String url, method;
         private JSONObject values;
+        private int num;
+        Boolean response;
 
-        public NetworkTask(String url, JSONObject values) {
+        public NetworkTask(String url, String method, Boolean response, JSONObject values, int num) {
             this.url = url;
+            Log.d("url", url);
+            this.method = method;
             this.values = values;
+            this.response = response;
+            this.num = num;
         }
 
         @Override
-        protected String doInBackground(Void... params) {
+        protected JSONObject doInBackground(Void... params) {
             Log.d("체크","doInBackground 진입");
-            String result; // 요청 결과를 저장할 변수.
+            JSONObject result; // 요청 결과를 저장할 변수.
             RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
-            result = requestHttpURLConnection.request(url, values); // 해당 URL 로부터 결과물을 얻어온다.
+            result = requestHttpURLConnection.request(url, method, response, values); // 해당 URL 로부터 결과물을 얻어온다.
             return result;
         }
 
         @Override
-        protected void onPostExecute(String s) {
+        protected void onPostExecute(JSONObject s) {
             super.onPostExecute(s); //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
-            if (s == null){
-                Log.d("체크","회원가입 실패");
+            temp = s;
+            System.out.println(temp);
+
+            // 아이디 중복결과
+            if (num == 1){
+                try {
+                    // "true" 이면 중복X
+                    checkIdResponse = s.getString("response");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-            else{
-                Log.d("체크","회원가입 성공");
-                // 로그인 화면으로
+            // 닉네임 중복결과
+            else if (num == 2){
+                try {
+                    // "true" 이면 중복X
+                    checkNickResponse = s.getString("response");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                // 아이디가 중복, 닉네임은 OK
+                if (!checkIdResponse.equals("true") && checkNickResponse.equals("true")) {
+                    Toast toast1 = Toast.makeText(getApplicationContext(),"중복된 아이디 입니다.", Toast.LENGTH_SHORT);
+                    toast1.show();
+                }
+                // 아이디는 OK, 닉네임이 중복
+                else if (checkIdResponse.equals("true") && !checkNickResponse.equals("true")){
+                    Toast toast2 = Toast.makeText(getApplicationContext(),"중복된 닉네임 입니다.", Toast.LENGTH_SHORT);
+                    toast2.show();
+                }
+                // 둘다 중복
+                else if (!checkIdResponse.equals("true") && !checkNickResponse.equals("true")){
+                    Toast toast3 = Toast.makeText(getApplicationContext(),"중복된 아이디와 닉네임 입니다.", Toast.LENGTH_SHORT);
+                    toast3.show();
+                }
+                // 둘다 중복아님
+                else if(checkIdResponse.equals("true") && checkNickResponse.equals("true")){
+                    // 회원가입
+                    NetworkTask networkTask3 = new NetworkTask(signup_url, "post", true, signup_info, 3);
+                    networkTask3.execute();
+                }
+            }
+            else if (num == 3){
+                try {
+                    checkSignupResponse = s.getString("status_code");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if (checkSignupResponse.equals("201")){
+                    Toast toast4 = Toast.makeText(getApplicationContext(),"회원가입에 성공하였습니다.", Toast.LENGTH_SHORT);
+                    toast4.show();
+                    Intent intent = new Intent(getApplicationContext(),User_login.class);
+                    startActivity(intent);
+                }
             }
         }
     }
