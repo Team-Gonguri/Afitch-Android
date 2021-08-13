63f5462e349d33f258f6f8397415d1caf1546b7f
박지원 <cute060500@naver.com>
2021-08-13 11:22:04 +0900

    AccessToken 인증 or 재발급

--- app/src/main/java/com/example/afitch/User_login.java
+++ app/src/main/java/com/example/afitch/User_login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -17,19 +18,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
-
-import java.util.concurrent.ExecutionException;
 
 public class User_login extends AppCompatActivity {
 
     public TextView goto_signup;
     public EditText edit_id, edit_pw;
     public Button goto_login;
-    String id, pw;
-    JSONObject temp;
+    String id, pw, accessToken, refreshToken, temp, accessExpr, refreshExpr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +38,6 @@ public class User_login extends AppCompatActivity {
         goto_signup = (TextView) findViewById(R.id.goto_signup);
         edit_id = (EditText) findViewById(R.id.login_id);
         edit_pw = (EditText) findViewById(R.id.login_pw);
-        temp = new JSONObject();
 
         goto_signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +50,7 @@ public class User_login extends AppCompatActivity {
         goto_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Log.d("체크","로그인 버튼 클릭확인");
                 id = edit_id.getText().toString();
                 pw = edit_pw.getText().toString();
@@ -94,7 +92,7 @@ public class User_login extends AppCompatActivity {
             JSONObject result;
             Log.d("체크","doInBackground 진입");
             RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
-            result = requestHttpURLConnection.request(url, method, response, values); // 해당 URL로 부터 결과물을 얻어온다.
+            result = requestHttpURLConnection.request(url, method, response, values, ""); // 해당 URL로 부터 결과물을 얻어온다.
             Log.d("체크", "result : " + result);
             return result;
         }
@@ -102,11 +100,23 @@ public class User_login extends AppCompatActivity {
         @Override
         protected void onPostExecute(JSONObject s) {
             super.onPostExecute(s); //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
-            temp = s;
             try {
                 if (s.getString("status_code").equals("200")){
                     Toast toast = Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.", Toast.LENGTH_SHORT);
                     toast.show();
+
+                    // access token, refresh token 저장
+                    temp = s.getString("response");
+                    JSONObject jsonObject = new JSONObject(temp);
+                    JSONObject access = new JSONObject(jsonObject.getString("access"));
+                    JSONObject refresh = new JSONObject(jsonObject.getString("refresh"));
+                    accessToken = access.getString("token");
+                    accessExpr = access.getString("expiredIn");
+                    refreshToken = refresh.getString("token");
+                    refreshExpr = access.getString("expiredIn");
+
+                    storeInFile();
+
                     Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                     startActivity(intent);
                 }
@@ -119,5 +129,23 @@ public class User_login extends AppCompatActivity {
             }
         }
     }
+
+    public void storeInFile(){
+
+        //앱이 종료되도 데이터를 사용할 수 있게 함
+        SharedPreferences sp = getSharedPreferences("file", MODE_PRIVATE);
+
+        //저장을 하기위해 editor를 이용하여 값을 저장시켜준다.
+        SharedPreferences.Editor editor = sp.edit();
+        //token 저장
+        editor.putString("accessToken", accessToken);
+        editor.putString("refreshToken", refreshToken);
+        editor.putString("accessExpr", accessExpr);
+        editor.putString("refreshExpr", accessExpr);
+
+        //최종 커밋
+        editor.commit();
+    }
+
 
 }
